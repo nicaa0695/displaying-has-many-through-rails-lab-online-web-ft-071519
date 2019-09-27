477184fa9d51d6e2c4017b3582128cd0c63a3fbc
Veronica Lopez <nicaa0695@gmail.com>
2019-09-27 20:01:15 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20190927181900_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.string :appointment_datetime
+      t.integer :doctor_id
+      t.integer :patient_id
+      t.string :datetime
+
+      t.timestamps
+    end
+  end
+end
